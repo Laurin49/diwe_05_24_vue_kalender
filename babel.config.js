d4613720a1685397df597a9d6aec66c3f18ad52a
Laurin49 <welter@diwe.info>
2024-05-15 17:58:34 +0200

    initial setup + statische Version

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,15 @@
+// module.exports = {
+//   presets: [
+//     '@vue/cli-plugin-babel/preset'
+//   ]
+// }
 module.exports = {
   presets: [
-    '@vue/cli-plugin-babel/preset'
-  ]
-}
+    [
+      "@babel/preset-env",
+      {
+        corejs: "core-js@3",
+      },
+    ],
+  ],
+};
