cf74bf906ebf5a9ddb51d4d124c6c5cf130dfcce
Laurin49 <welter@diwe.info>
2024-05-15 18:32:30 +0200

    dev01 Components

---
+++ src/components/CalendarDay.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="card" v-for="i in 7" :key="i">
+    <div class="card-header text-center" role="button">
+      <strong>Montag</strong>
+    </div>
+    <div class="card-body">
+      <div id="calendar-day">
+        <CalendarEvent />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import CalendarEvent from "./CalendarEvent.vue";
+export default {
+  name: "CalendarDay",
+  components: {
+    CalendarEvent,
+  }
+};
+</script>
+<style scoped>
+</style>
---
+++ src/components/CalendarWeek.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="calender-week">
+    <div class="card-group">
+      <CalendarDay />
+    </div>
+  </div>
+</template>
+<script>
+import CalendarDay from "./CalendarDay.vue";
+export default {
+  name: "CalendarWeek",
+  components: {
+    CalendarDay,
+  }
+};
+</script>
+<style scoped>
+</style>
