bf30500a2b3b0c1adbf8a90cedff2cf6ec5ce002
Laurin49 <welter@diwe.info>
2024-05-19 16:26:35 +0200

    dev05 neuer eintrag calendarsetting ...

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div class="container-fluid mt-5">
     <div class="row">
       <div class="col-12">
-        <CalendarWeek />
+        <!-- <CalendarWeekAsList />
+        <CalendarWeek /> -->
+        <keep-alive>
+          <component :is="activeView"/>
+        </keep-alive>
       </div>
     </div>
     <div class="row mt-3">
@@ -12,28 +16,58 @@
       <div class="col-2 offset-2">
         <div class="float-end">
           <!-- Mit dem Button blenden wir die Calendar-Settings-Component ein bzw. aus. -->
-          <button class="btn btn-lg mb-2">
+          <button
+            class="btn btn-lg mb-2"
+            :class="buttonSettingsClasses"
+            @click="toggleDisplaySettings()"
+          >
             <i class="fas fa-cogs"></i>
           </button>
         </div>
-        <CalendarSettings />
+        <CalendarSettings v-if="displaySettings" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+// import { defineAsyncComponent } from "vue";
+import Store from "./store";
 import CalendarWeek from "./components/CalendarWeek.vue";
+import CalendarWeekAsList from "./components/CalendarWeekAsList.vue";
 import CalendarEntry from "./components/CalendarEntry.vue";
 import CalendarSettings from "./components/CalendarSettings.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
+    CalendarWeekAsList,
     CalendarWeek,
     CalendarEntry,
     CalendarSettings,
-  }
-}
+    // CalendarSettings: defineAsyncComponent(() => {
+    //   import(/*webpackChunkName: 'CalendarSettingsComponent' */ 
+    //   "./components/CalendarSettings.vue");
+    // }),
+  },
+  data() {
+    return {
+      displaySettings: false,
+    };
+  },
+  computed: {
+    buttonSettingsClasses() {
+      return this.displaySettings ? ["btn-success"] : ["btn-outline-success"];
+    },
+    activeView() {
+      return Store.getters.activeView();
+    }
+  },
+  methods: {
+    toggleDisplaySettings() {
+      this.displaySettings = !this.displaySettings;
+    },
+  },
+};
 </script>
 
 <style>
